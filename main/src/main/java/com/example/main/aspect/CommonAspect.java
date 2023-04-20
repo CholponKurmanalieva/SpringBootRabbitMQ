c39b7ebf63b8bd5c7ed4a55bfb917b3835c4e16f
CholponKurmanalieva <ch7kurmanalieva@gmail.com>
2023-04-20 19:23:41 +0600

    deleted aspect

--- main/src/main/java/com/example/main/aspect/CommonAspect.java
+++ main/src/main/java/com/example/main/aspect/CommonAspect.java
@@ -1,8 +1,6 @@
 package com.example.main.aspect;
 
-import com.example.main.entity.AppUser;
 import lombok.extern.log4j.Log4j;
-import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
@@ -12,11 +10,8 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Log4j
 public class CommonAspect {
-//    @Pointcut("execution(public String sendToEmail(com.example.main.entity.AppUser))")
-//    private void saveLogger(){}
+    @Pointcut("execution(public String sendToEmail(com.example.main.entity.AppUser))")
+    private void saveLogger(){}
 
-    @Around("execution(public String sendToEmail(com.example.main.entity.AppUser))")
-    public void createAppUser() {
-        log.info("Hello");
-    }
+    //TODO
 }
